362f547de9ffe02298e6ac270dd559c4c1eb1502
Jekel8888 <52115322+Jekel8888@users.noreply.github.com>
2019-11-16 19:36:16 -0700

    got buttons all set up and working

--- src/components/Features.js
+++ src/components/Features.js
@@ -9,6 +9,11 @@ class Features extends React.Component {
   // });
 
   render() {
+    const processor = this.props.features.Processor;
+    const OS = this.props.features["Operating System"];
+    const VC = this.props.features["Video Card"];
+    const Display = this.props.features.Display;
+
     console.log(this.props.features);
     console.log(this.props.test);
     return (
@@ -16,9 +21,27 @@ class Features extends React.Component {
         <fieldset className="feature" key={this.featureHash}>
           <legend className="feature__name">
             <h3>{this.props.test}</h3>
-            <Options />
+            <Options
+              title="processor"
+              name={[processor[0].name, processor[1].name]}
+              cost={[processor[0].cost, processor[1].cost]}
+            />
+            <Options
+              title="OperSys"
+              name={[OS[0].name, OS[1].name]}
+              cost={[OS[0].cost, OS[1].cost]}
+            />
+            <Options
+              title="VideoCard"
+              name={[VC[0].name, VC[1].name]}
+              cost={[VC[0].cost, VC[1].cost]}
+            />
+            <Options
+              title="Disp"
+              name={[Display[0].name, Display[1].name]}
+              cost={[Display[0].cost, Display[1].cost]}
+            />
           </legend>
-          <Options />
         </fieldset>
       </form>
     );
--- src/components/Options.js
+++ src/components/Options.js
@@ -15,28 +15,40 @@ class Options extends React.Component {
     this.props.features.map(choice => choice);
   };
   render() {
+    const name = this.props.name;
+    const cost = this.props.cost;
+    const nameKeyOne = this.props.name[0].replace(" ", "-");
+    const nameKeyTwo = this.props.name[1].replace(" ", "-");
     return (
       <div className="summary__option" key={this.props.id}>
         {/* <div className="summary__option__label">{this.props.feature} </div>
         <div className="summary__option__value">{this.selectedOption}</div> */}
-        <label className="1" htmlFor={this.props.id}>
-          {this.props.features}
-          <input
-            type="radio"
-            id={this.props.id}
-            value={this.props.features}
-            name={this.props.name}
-          ></input>
+        {/* <label className="1" htmlFor={this.props.id}>
+          {this.props.features} */}
+        <input
+          type="radio"
+          id={`${this.props.title}1`}
+          // value={this.props.features}
+          name={this.props.title}
+          value={`${this.props.title}1`}
+        />
+        <label htmlFor={`${this.props.title}1`}>
+          {name[0]}, {cost[0]}
         </label>
-        <label className="2" htmlFor={this.props.id}>
-          {this.props.features}
-          <input
-            type="radio"
-            id={this.props.id}
-            value={this.props.features}
-            name={this.props.name}
-          ></input>
+        {/* </label> */}
+        {/* <label className="2" htmlFor={this.props.id}>
+          {this.props.features} */}
+        <input
+          type="radio"
+          id={`${this.props.title}2`}
+          // value={this.props.features}
+          name={this.props.title}
+          value={`${this.props.title}2`}
+        />
+        <label htmlFor={`${this.props.title}2`}>
+          {name[1]}, {cost[1]}
         </label>
+        {/* </label> */}
       </div>
     );
   }
